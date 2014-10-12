c0749102901c956d0fd5bd97d112c2eccfc8071b
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-10-12 13:34:22 +0200

    Initial commit

---
+++ Doom/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Doom
+//
+//  Created by Gabriele on 12/10/14.
+//  Copyright (c) 2014 Ga2 & co. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+}
+
